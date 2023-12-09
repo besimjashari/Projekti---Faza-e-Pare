99da9bd701340fa03520e53b29bbb13e38f4d9b7
blenard1 <blenardtahiraj1@gmail.com>
2023-12-10 00:49:33 +0100

    minor update

--- switchform.js
+++ switchform.js
@@ -44,3 +44,7 @@ function showRegisterForm() {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("registerForm").style.display = "block";
 }
+function showLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("registerForm").style.display = "none";
+}
